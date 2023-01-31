467a699d5ae7b5cd3c1da970da1c3f4b51409332
paulhmurray <paulthemurray@gmail.com>
2023-01-31 14:22:56 +1100

    push for new OS install

---
+++ koreader/history.lua
@@ -0,0 +1,6 @@
+return {
+    [1] = {
+        ["file"] = "/home/paulm/.config/koreader/help/quickstart-en-v2022.11.html",
+        ["time"] = 1674442788,
+    },
+}
---
+++ koreader/settings/battery_stats.lua
@@ -0,0 +1,27 @@
+-- we can read Lua syntax here!
+return {
+    ["awake"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+    ["awake_state"] = {
+        ["percentage"] = 0,
+        ["timestamp"] = 373773425990,
+    },
+    ["charging"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+    ["charging_state"] = {
+        ["percentage"] = 0,
+        ["timestamp"] = 373773426062,
+    },
+    ["discharging"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+    ["sleeping"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+}
